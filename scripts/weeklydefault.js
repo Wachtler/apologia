8777b6794021ec4d91eb46a996a03b40b240522d
Riker Wachtler <riker.wachtler@gmail.com>
2016-09-21 18:57:09 -0700

    thankee conor o'brien for help
    he refactored this, 10/10

--- scripts/weeklydefault.js
+++ scripts/weeklydefault.js
@@ -12,29 +12,34 @@ function addLoadEvent(func) {
   }
 }
 
-function getWeekNumber(){
-	date = new Date()
-	
-	var x = 0;
-	
-	if (date < new Date("2017","0", "23")){x = 15;}
-	if (date < new Date("2017","0", "17")){x = 14;}
-	if (date < new Date("2017","0", "9")){x = 13;}
-	if (date < new Date("2016","11", "12")){x = 12;}
-	if (date < new Date("2016","11", "5")){x = 11;}
-	if (date < new Date("2016","10", "28")){x = 10;}
-	if (date < new Date("2016","10", "14")){x = 9;}
-	if (date < new Date("2016","10", "7")){x = 8;}
-	if (date < new Date("2016","9", "31")){x = 7;}
-	if (date < new Date("2016","9", "24")){x = 6;}
-	if (date < new Date("2016","9", "17")){x = 5;}
-	if (date < new Date("2016","9", "10")){x = 4;}
-	if (date < new Date("2016","9", "3")){x = 3;}
-	if (date < new Date("2016","8", "26")){x = 2;}
-	if (date < new Date("2016","8", "19")){x = 1;}
-	if (date < new Date("2016","8", "12")){x = 0;}
-	
-	return x;
+const getWeekNumber = () => {
+    let currentDate = new Date();
+    let dates = [
+        [ "2016", "8",  "12" ],
+        [ "2016", "8",  "19" ],
+        [ "2016", "8",  "26" ],
+        [ "2016", "9",  "3"  ],
+        [ "2016", "9",  "10" ],
+        [ "2016", "9",  "17" ],
+        [ "2016", "9",  "24" ],
+        [ "2016", "9",  "31" ],
+        [ "2016", "10", "7"  ],
+        [ "2016", "10", "14" ],
+        [ "2016", "10", "28" ],
+        [ "2016", "11", "5"  ],
+        [ "2016", "11", "12" ],
+        [ "2017", "0",  "9"  ],
+        [ "2017", "0",  "17" ],
+        [ "2017", "0",  "23" ]
+    ];
+    for(let i = 0; i < dates.length; i++){
+        let [year, month, day] = dates[i];
+        if(currentDate < new Date(year, month, day))
+            return i + 1;
+    }
+    
+    // fail-case--zero
+    return 0;
 }
 
 addLoadEvent(function (){document.getElementById("embeddedlist").src="./embed/weeklyembed" + getWeekNumber() + ".html"});
